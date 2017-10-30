e3b4536042f331a9af621a8a7c5763968d513754
Abel Osorio <abel@kogimobile.com>
2017-10-30 07:48:15 -0500

    [feat]: Add three more test to make a 100% coverage

--- FizzBuzzTest/ViewControllersTest/GameViewControllerUnitTests.swift
+++ FizzBuzzTest/ViewControllersTest/GameViewControllerUnitTests.swift
@@ -54,6 +54,13 @@ class GameViewControllerUnitTests: XCTestCase {
         XCTAssertEqual(newScore, 3)
     }
 
+    func testFizzIncrementScoreByIBAction() {
+        viewController.game.score = 2
+        viewController.fizzButtonTapped(UIButton())
+        let newScore = viewController.gameScore
+        XCTAssertEqual(newScore, 3)
+    }
+
     func testBuzzIncrementScore() {
         viewController.game.score = 4
         viewController.play(move: .buzz)
@@ -61,9 +68,23 @@ class GameViewControllerUnitTests: XCTestCase {
         XCTAssertEqual(newScore, 5)
     }
 
+    func testBuzzIncrementScoreByIBAction() {
+        viewController.game.score = 4
+        viewController.buzzbuttonTapped(UIButton())
+        let newScore = viewController.gameScore
+        XCTAssertEqual(newScore, 5)
+    }
+
     func testFizzBuzzIncrementScore() {
         viewController.game.score = 14
         viewController.play(move: .fizzBuzz)
+        let newScore = viewController.gameScore
+        XCTAssertEqual(newScore, 15)
+    }
+
+    func testFizzBuzzIncrementScoreByIBAction() {
+        viewController.game.score = 14
+        viewController.fizzBuzzbuttonTapped(UIButton())
         let newScore = viewController.gameScore
         XCTAssertEqual(newScore, 15)
     }
